850fade7ab8f954249aa4bd985beef7b96491fc9
fangs <kevin@zipfworks.com>
2014-10-21 11:48:33 -0700

    adding authentications and channels per node

--- src/main/scala/com/zipfworks/sprongo/macros/MacroDB.scala
+++ src/main/scala/com/zipfworks/sprongo/macros/MacroDB.scala
@@ -2,13 +2,19 @@ package com.zipfworks.sprongo.macros
 
 import akka.actor.ActorSystem
 import reactivemongo.api.{MongoConnection, MongoDriver}
+import reactivemongo.core.nodeset.Authenticate
 
-class MacroDB(nodes: Seq[String], db: String)(implicit system: ActorSystem) {
+class MacroDB(nodes: Seq[String], db: String, authentications: Seq[Authenticate] = Seq.empty, nbChannelsPerNode: Int = 10)
+             (implicit system: ActorSystem) {
 
   import system.dispatcher
 
   val mongoDriver: MongoDriver         = MongoDriver(system)
-  val mongoConnection: MongoConnection = mongoDriver.connection(nodes = nodes)
+  val mongoConnection: MongoConnection = mongoDriver
+    .connection(nodes = nodes, authentications = authentications, nbChannelsPerNode = nbChannelsPerNode, name = None)
 
   implicit val defaultDB = mongoConnection(db)
+
 }
+
+
---
+++ src/main/scala/com/zipfworks/sprongo/macros/Model.scala
@@ -0,0 +1,5 @@
+package com.zipfworks.sprongo.macros
+
+trait Model[T] {
+  def _id: T
+}
