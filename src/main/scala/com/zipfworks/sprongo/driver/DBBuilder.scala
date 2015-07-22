450299fb855e54395bae5842b94d1e0d00458b75
kevin <kevin@zipfworks.com>
2015-07-22 13:27:36 -0700

    update db builder

--- src/main/scala/com/zipfworks/sprongo/driver/DBBuilder.scala
+++ src/main/scala/com/zipfworks/sprongo/driver/DBBuilder.scala
@@ -1,22 +1,23 @@
 package com.zipfworks.sprongo.driver
 
 import com.zipfworks.sprongo.{CollectionDAO, Model}
-import reactivemongo.api.DefaultDB
+import reactivemongo.api.{MongoConnection, DefaultDB}
 import spray.json.RootJsonFormat
 
 import scala.concurrent.ExecutionContext
 
 trait DBBuilder {
 
-  def dbName: String
-  def driver: SprongoDriver
+  val dbName: String
+  val connection: MongoConnection
 
-  private def executionContext: ExecutionContext = driver.driver.system.dispatcher
-
-  def defaultDB: DefaultDB = driver.connection(dbName)(executionContext)
+  private def executionContext: ExecutionContext = connection.actorSystem.dispatcher
+  private def defaultDB: DefaultDB = connection(dbName)(executionContext)
 
   def getCollection[M <: Model](collName: String)(implicit js: RootJsonFormat[M]) = {
     new CollectionDAO[M](collName)(ec = executionContext, jsFormat = js, db = defaultDB)
   }
 
 }
+
+
